4913fbf4d07193ad5a0f40dfa09af0c233e0fe66
Longsmoke <nikyprado@hotmail.com>
2016-03-02 11:45:57 -0300

    ENTRADA DE DADOS

---
+++ O Biladeiro e as Feminazi/Biladeiro.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace O_Biladeiro_e_as_Feminazi
+{
+    class Biladeiro
+    {
+
+        public int x;
+        public int y;
+
+        public int vida;
+        public int energia;
+    }
+}
