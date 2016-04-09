6e0252dbbecaab94efd4f77cbecd6df90369605e
Longsmoke <nikyprado@hotmail.com>
2016-04-09 02:20:26 -0300

    a

---
+++ Stick RPG Fight/Personagens/Sangue.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+using Microsoft.Xna.Framework.Input;
+using Microsoft.Xna.Framework.Audio;
+using Microsoft.Xna.Framework.Media;
+using Microsoft.Xna.Framework.Content;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Stick_RPG_Fight
+{
+    class Sangue
+    {
+        public Rectangle sangueR = new Rectangle();
+        public int Vx;
+        public int VariavelX = 0;
+        public int Vy;
+        public int g = 0;
+        public bool ON = false;
+        public int contagem = 0;
+    }
+}
